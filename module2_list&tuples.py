baafd8a6d6c035bef6412bf7d4531a62045d39b9
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-24 10:40:59 -0600

    Update module2_list&tuples.py
    Completed the list and tuple section

--- module2_list&tuples.py
+++ module2_list&tuples.py
@@ -1,1 +1,35 @@
+# tuples are within paranthesis
+tuple1 = ("disco",10,1.2)
+tuple[0] 
+tuple[-1] 
+tuple2 = tuple1 + ("hardrock")
+# slice tuple
+tuple1[0:3] # 0 to 2
+len(tuple2)
+# tuples are immutable
+rated = sorted(tuple2)
+nt = (1,2,("pop","index"),(3,4))
+# can have tuple inside tuple
+nt[2][1] # this will give "index"
+nt[2][1][0] # this will give "i"
 
+
+# list is by square brackets. can next tuple and list
+# but list are muttable
+# can be accessed by index same as tuple
+
+list1 =["hello",4,(1,2)]
+list1.extend(["bini", 1.2] #this adds two elements
+list1.append(["pop,10]) # this adds one ele
+# del(list1[0]) delete first element
+"hard rock".split() # this will convert string to list
+
+# list.insert(2,6) ??
+# list.pop() ?/
+# list.remove
+# list.sort(reverse=True)
+
+# tuple.count() tuple.index(value) sum(tuple) 
+              
+
+             
