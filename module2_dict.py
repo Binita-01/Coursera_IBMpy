24fc342137c684b24453e3633fe8df1eefd98bb4
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-24 14:01:25 -0600

    Create module2_dict.py
    finished dict module

---
+++ module2_dict.py
@@ -0,0 +1,10 @@
+# dictionary: use curly bracket
+# keys are immutable and unique
+# values can be mutable and duplicates
+# each key and value is speerated by comma
+
+# dict1.keys() this will give the keys
+# dict1.values() will give values
+
+# Verify the key is in the dictionary
+# The Bodyguard' in release_year_dict
