e07d944f0c689bf8940465f6f618f34d5bf03d21
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-24 14:06:58 -0600

    Create module2_sets.py
    sets finished

---
+++ module2_sets.py
@@ -0,0 +1,24 @@
+# sets are a type of collection
+# sets are unordered: thus can't be accessed through index
+# sets only have unique elements
+# 
+
+# set {"pop","rock"}
+# even if there are duplicates when creating, once it has been created there will be no duplicates. 
+# list to set by using set(list1)
+
+# to add at the end
+# set1.add("nc")
+# can add same thing twice but nothing will happen
+# set.remove("nc") : to remove an element
+
+# verify by : 
+# "nc" in set 
+# true if it is in the set
+
+# mathematical set operations
+
+# set1 & set2 gives union of two sets OR use following
+# set1.union(set2)
+
+# set1.issubset(set2) 
