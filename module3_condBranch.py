b9164d5d99c72dab9ae846e8a1b5261664299816
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-25 15:02:25 -0600

    Update module3_condBranch.py
    completed the module

--- module3_condBranch.py
+++ module3_condBranch.py
@@ -1,1 +1,13 @@
+if (age>18):
+  print("you will enter")
+elif(age==18):
+  print("you are 18")
 
+# LOGIC operators
+# and
+# or
+
+is_do_not_disturb = True
+if not is_do_not_disturb: 
+  print("new message received")
+  
