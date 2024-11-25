3e5424b75a6bedd9a015c08283ee782eb8fb657a
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-25 15:25:37 -0600

    Create module3_functions.py
    completed the module

---
+++ module3_functions.py
@@ -0,0 +1,22 @@
+def f1(input): 
+  output = input +1;
+  return input
+
+aa = f1(2)
+
+# built in functions
+album_1 = sorted(album_ratings)
+# doesn't change album_ratings
+
+# built in methods
+alubum_ratings.sort()
+# changes the album_ratings directly
+
+def mult(a,b):
+  c= a*b
+  return c
+
+res = mult(2,"binitakc")
+
+
+# enumerate: value, index
