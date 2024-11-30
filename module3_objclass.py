9f2e6cecba8ea44d2b80a8366a0e0ff5f73b7730
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-30 15:42:57 -0600

    Update module3_objclass.py
    WIP

--- module3_objclass.py
+++ module3_objclass.py
@@ -1,1 +1,18 @@
 
+class car:
+  max_speed = 120
+  # this attribute is shared by all instances
+  # __init__ method aslo known as the constructor
+  # self parameter is the first parameter of the constructor, referring to the instance being crreated
+  def __init__(self,speed,mileage):
+    self.spedd = speed
+    self.mileage=mileage
+  def accelerate(self,acceleration):
+    if self.speed + acceleration <= max_speed:
+      self.speed += acceleration
+    else:
+      self.speed= max_speed
+      
+  def get_speed(self):
+    return self.speed
+    
